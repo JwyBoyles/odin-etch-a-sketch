79e961c70f3a949b538e69325f8821fb514ef3ec
jakewhy <jwyboyles@gmail.com>
2022-08-02 15:16:28 -0500

    fix grid size

--- javascript.js
+++ javascript.js
@@ -1,55 +1,14 @@
 const container = document.querySelector('#container');
 
-const divOne = document.createElement('div');
-const divTwo = document.createElement('div');
-const divThree = document.createElement('div');
-const divFour = document.createElement('div');
-const divFive = document.createElement('div');
-const divSix = document.createElement('div');
-const divSeven = document.createElement('div');
-const divEight = document.createElement('div');
-const divNine = document.createElement('div');
-const divTen = document.createElement('div');
-const divEleven = document.createElement('div');
-const divTwelve = document.createElement('div');
-const divThirteen = document.createElement('div');
-const divFourteen = document.createElement('div');
-const divFifteen = document.createElement('div');
-const divSixteen = document.createElement('div');
+function createGrid() {
+    let div = document.createElement('div');
+    container.appendChild(div);
+    div.classList.add('gridItem')
+}
 
-container.appendChild(divOne);
-container.appendChild(divTwo);
-container.appendChild(divThree);
-container.appendChild(divFour);
-container.appendChild(divFive);
-container.appendChild(divSix);
-container.appendChild(divSeven);
-container.appendChild(divEight);
-container.appendChild(divNine);
-container.appendChild(divTen);
-container.appendChild(divEleven);
-container.appendChild(divTwelve);
-container.appendChild(divThirteen);
-container.appendChild(divFourteen);
-container.appendChild(divFifteen);
-container.appendChild(divSixteen);
-
-divOne.classList.add('gridItem');
-divTwo.classList.add('gridItem');
-divThree.classList.add('gridItem');
-divFour.classList.add('gridItem');
-divFive.classList.add('gridItem');
-divSix.classList.add('gridItem');
-divSeven.classList.add('gridItem');
-divEight.classList.add('gridItem');
-divNine.classList.add('gridItem');
-divTen.classList.add('gridItem');
-divEleven.classList.add('gridItem');
-divTwelve.classList.add('gridItem');
-divThirteen.classList.add('gridItem');
-divFourteen.classList.add('gridItem');
-divFifteen.classList.add('gridItem');
-divSixteen.classList.add('gridItem');
+for (let p = 0; p < 64; ++p) {
+    createGrid()
+}
 
 let gridItem = document.getElementsByClassName("gridItem");
 
@@ -58,5 +17,6 @@ gridItem[i].addEventListener('mouseenter', () => {
   gridItem[i].classList.add('gridChosen');
 });
 }
+
 
 
